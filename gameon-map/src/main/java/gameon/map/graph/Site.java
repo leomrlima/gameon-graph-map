af5ef745012f94f3264f7996bae552535e37fa35
Otavio Santana <otaviopolianasantana@gmail.com>
2017-09-17 21:24:22 -0300

    Fixes equals and hashcode

--- gameon-map/src/main/java/gameon/map/graph/Site.java
+++ gameon-map/src/main/java/gameon/map/graph/Site.java
@@ -4,187 +4,183 @@ import org.jnosql.artemis.Column;
 import org.jnosql.artemis.Entity;
 import org.jnosql.artemis.Id;
 
+import java.util.Objects;
+
 @Entity
 public class Site {
 
-	@Id
-	private String id;
+    @Id
+    private String id;
 
-	@Column
-	private String name;
+    @Column
+    private String name;
 
-	/**
-	 * Connection type
-	 */
-	@Column
-	private String connectionType;
+    /**
+     * Connection type
+     */
+    @Column
+    private String connectionType;
 
-	/**
-	 * Connection target, usually a URL
-	 */
-	@Column
-	private String connectionTarget;
+    /**
+     * Connection target, usually a URL
+     */
+    @Column
+    private String connectionTarget;
 
-	/**
-	 * (Optional) A token used for mutual identification between the room and
-	 * the mediator during the initial handshake when the connection is
-	 * established
-	 */
-	@Column
-	private String connectionToken;
+    /**
+     * (Optional) A token used for mutual identification between the room and
+     * the mediator during the initial handshake when the connection is
+     * established
+     */
+    @Column
+    private String connectionToken;
 
-	/**
-	 * (Optional) Human-friendly room name
-	 */
-	@Column
-	private String fullName;
+    /**
+     * (Optional) Human-friendly room name
+     */
+    @Column
+    private String fullName;
 
-	/**
-	 * (Optional) Player-friendly room description (140 characters)
-	 */
-	@Column
-	private String description;
+    /**
+     * (Optional) Player-friendly room description (140 characters)
+     */
+    @Column
+    private String description;
 
-	@Column
-	private String owner;
+    @Column
+    private String owner;
 
-	@Column
-	private long x;
+    @Column
+    private long x;
 
-	@Column
-	private long y;
+    @Column
+    private long y;
 
-	@Column
-	private boolean empty;
+    @Column
+    private boolean empty;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
-		return result;
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Site other = (Site) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (owner == null) {
-			if (other.owner != null)
-				return false;
-		} else if (!owner.equals(other.owner))
-			return false;
-		return true;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getConnectionType() {
+        return connectionType;
+    }
 
-	public String getConnectionType() {
-		return connectionType;
-	}
+    public void setConnectionType(String connectionType) {
+        this.connectionType = connectionType;
+    }
 
-	public void setConnectionType(String connectionType) {
-		this.connectionType = connectionType;
-	}
+    public String getConnectionTarget() {
+        return connectionTarget;
+    }
 
-	public String getConnectionTarget() {
-		return connectionTarget;
-	}
+    public void setConnectionTarget(String connectionTarget) {
+        this.connectionTarget = connectionTarget;
+    }
 
-	public void setConnectionTarget(String connectionTarget) {
-		this.connectionTarget = connectionTarget;
-	}
+    public String getConnectionToken() {
+        return connectionToken;
+    }
 
-	public String getConnectionToken() {
-		return connectionToken;
-	}
+    public void setConnectionToken(String connectionToken) {
+        this.connectionToken = connectionToken;
+    }
 
-	public void setConnectionToken(String connectionToken) {
-		this.connectionToken = connectionToken;
-	}
+    public String getFullName() {
+        return fullName;
+    }
 
-	public String getFullName() {
-		return fullName;
-	}
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public String getOwner() {
+        return owner;
+    }
 
-	public String getOwner() {
-		return owner;
-	}
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 
-	public void setOwner(String owner) {
-		this.owner = owner;
-	}
+    public long getX() {
+        return x;
+    }
 
-	public long getX() {
-		return x;
-	}
+    public void setX(long x) {
+        this.x = x;
+    }
 
-	public void setX(long x) {
-		this.x = x;
-	}
+    public long getY() {
+        return y;
+    }
 
-	public long getY() {
-		return y;
-	}
+    public void setY(long y) {
+        this.y = y;
+    }
 
-	public void setY(long y) {
-		this.y = y;
-	}
+    public boolean isEmpty() {
+        return empty;
+    }
 
-	public boolean isEmpty() {
-		return empty;
-	}
+    public void setEmpty(boolean empty) {
+        this.empty = empty;
+    }
 
-	public void setEmpty(boolean empty) {
-		this.empty = empty;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Site site = (Site) o;
+        return Objects.equals(name, site.name) &&
+                Objects.equals(owner, site.owner);
+    }
 
-	@Override
-	public String toString() {
-		return "Site [" + (id != null ? "id=" + id + ", " : "") + (name != null ? "name=" + name + ", " : "")
-				+ (connectionType != null ? "connectionType=" + connectionType + ", " : "")
-				+ (connectionTarget != null ? "connectionTarget=" + connectionTarget + ", " : "")
-				+ (connectionToken != null ? "connectionToken=" + connectionToken + ", " : "")
-				+ (fullName != null ? "fullName=" + fullName + ", " : "")
-				+ (description != null ? "description=" + description + ", " : "")
-				+ (owner != null ? "owner=" + owner + ", " : "") + "x=" + x + ", y=" + y + ", empty=" + empty + "]";
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, owner);
+    }
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Site{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", connectionType='").append(connectionType).append('\'');
+        sb.append(", connectionTarget='").append(connectionTarget).append('\'');
+        sb.append(", connectionToken='").append(connectionToken).append('\'');
+        sb.append(", fullName='").append(fullName).append('\'');
+        sb.append(", description='").append(description).append('\'');
+        sb.append(", owner='").append(owner).append('\'');
+        sb.append(", x=").append(x);
+        sb.append(", y=").append(y);
+        sb.append(", empty=").append(empty);
+        sb.append('}');
+        return sb.toString();
+    }
 }
